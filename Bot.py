dd5ebbf967553538ca137c02fd42f68c5c0efd1c
Colluk <virus1605@mail.ru>
2021-01-17 23:15:25 +0300

    version 3

--- Bot.py
+++ Bot.py
@@ -4,23 +4,28 @@ import config
 
 bot = telebot.TeleBot(config.token)
 
-keyboard = telebot.types.ReplyKeyboardMarkup(True)
-keyboard.row('привет', 'здорова', 'здравствуйте')
-
+keyboard = telebot.types.ReplyKeyboardMarkup(True, True)
+keyboard.row(*config.phrases_first_button)
+keyboard2 = telebot.types.ReplyKeyboardMarkup(True, True)
+keyboard2.row(config.phrases_second_button)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, config.fisrt_phrase,
+    bot.send_message(message.chat.id, config.phrases['word after start'],
                      reply_markup=keyboard)
-
 
 @bot.message_handler(content_types=['text'])
 def send_answer(message):
-    pharases = config.pharases
-    if message.text.lower() in pharases:
-        bot.send_message(message.chat.id, config.hello)
+    if message.text.lower() in config.phrases_first_button:
+        bot.send_message(message.chat.id, config.phrases['say hello'])
+        bot.send_message(message.chat.id, config.phrases['ask me about strong'],
+                         reply_markup=keyboard2)
+    elif message.text.lower() == config.phrases_second_button:
+        bot.send_message(message.chat.id, config.phrases['I know where strong'])
+        bot.send_message(message.chat.id,
+                         config.phrases['More questions'])
     else:
-        bot.send_message(message.chat.id, config.i_dont_know)
+        bot.send_message(message.chat.id, config.phrases['i do not know'])
 
 
 bot.polling()
